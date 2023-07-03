e62246675f0b6a8ecfbcb532f565e936cd5c0b85
NadaHe <dodyhesham2001@gmail.com>
2023-07-03 15:30:09 +0300

    Adding ternary

--- control_flow.py
+++ control_flow.py
@@ -30,3 +30,32 @@ else:
 
   print(f"Hi {uName} Because U R From {uCountry}")
   print(f"The Course \"{cName}\" Price Is: ${cPrice - 30}")
+
+print("=======================================================")
+
+# ----------------------------------
+# -- Ternary Conditional Operator --
+# ----------------------------------
+
+country = "A"
+
+if country == "Egypt" : print(f"The Weather in {country} Is 15")
+elif country == "KSA" : print(f"The Weather in {country} Is 30")
+else : print("Country is Not in The List")
+
+# Short If
+
+movieRate = 18
+age = 18
+
+if age < movieRate :
+
+  print("Movie S Not Good 4U") # Condition If True
+
+else :
+
+  print("Movie S Good 4U And Happy Watching") # Condition If False
+
+print("Movie S Not Good 4U" if age < movieRate else "Movie S Good 4U And Happy Watching")
+
+# Condition If True | If Condition | Else | Condition If False
